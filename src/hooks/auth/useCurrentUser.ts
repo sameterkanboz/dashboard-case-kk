2a3d09db60d33cb547fc89b09aa689da8eba1a07
Samet Erkan Boz <71569375+sameterkanboz@users.noreply.github.com>
2024-04-22 13:12:32 +0300

    Update useCurrentUser.ts

--- src/hooks/auth/useCurrentUser.ts
+++ src/hooks/auth/useCurrentUser.ts
@@ -1,6 +1,5 @@
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
-import { authService } from "../../services";
 import { User } from "../../types/user";
 
 export const useCurrentUser = () => {
@@ -13,20 +12,5 @@ export const useCurrentUser = () => {
     }
   }, []);
 
-  const refetchUser = async (userId: string) => {
-    const userInfo = await authService.getMe(userId);
-    const currentUser = Cookies.get("currentUser");
-
-    if (userInfo && currentUser) {
-      const newUser = {
-        ...JSON.parse(currentUser),
-        username: userInfo.username,
-        avatar: userInfo.avatar,
-      };
-      Cookies.set("currentUser", JSON.stringify(newUser));
-      setUser(newUser);
-    }
-  };
-
-  return { user, refetchUser };
+  return { user };
 };
